f0a15cdbf9fa967a3ee4dd9a38db4b884cea6365
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-01-28 22:38:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-//your code here
+let btn =document.getElementById("addTodoBtn");
+let list=document.getElementById("todoList");
+function addTask() {
+  let input=document.getElementById("newTodoInput").value;
+	let li=document.createElement("li");
+	li.setAttribute("id","task");
+	li.textContent=input;
+	if(input!=""){
+		list.appendChild(li);
+	}else{
+		alert("Empty Task");
+	}
 
+}
